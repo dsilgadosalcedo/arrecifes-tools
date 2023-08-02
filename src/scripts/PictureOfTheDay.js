b9ac488526c7b546424f87051bfeed0823243a07
David Silgado <dsilgadosalcedo@gmail.com>
2023-08-01 20:46:00 -0500

    linked to apodapi successfully

--- src/scripts/PictureOfTheDay.js
+++ src/scripts/PictureOfTheDay.js
@@ -1,40 +1,20 @@
-export function getApodData() {
-  // Create a new XMLHttpRequest object
-  let xhr = new XMLHttpRequest();
+const apiKey = "kmQOP13MoJhnRftKtbw8wEjK04O4xI6Dls8R0dc6";
+const apiUrl = "https://api.nasa.gov/planetary/apod?api_key=" + apiKey;
 
-  // Set the URL to the APOD API endpoint with your apikey
-  let url = "https://api.nasa.gov/planetary/apod?api_key=kmQOP13MoJhnRftKtbw8wEjK04O4xI6Dls8R0dc6";
+export async function getApodData() {
+  try {
+    const response = await fetch(apiUrl);
+    const rateLimitRemaining = response.headers.get("X-RateLimit-Remaining");
 
-  // Open the connection
-  xhr.open("GET", url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
 
-  // Send the request
-  xhr.send();
-
-  // Return a Promise that resolves with the data and headers
-  return new Promise(function(resolve, reject) {
-    xhr.onload = function() {
-      // Check if the status is OK
-      if (xhr.status == 200) {
-        // Get the X-RateLimit-Remaining header value
-        let rateLimitRemaining = xhr.getResponseHeader("X-RateLimit-Remaining");
-        // Parse the response data as JSON
-        let data = JSON.parse(xhr.responseText);
-        // Resolve the Promise with the data and headers
-        resolve({
-          date: data.date,
-          explanation: data.explanation,
-          hdurl: data.hdurl,
-          media_type: data.media_type,
-          service_version: data.service_version,
-          title: data.title,
-          url: data.url,
-          rateLimitRemaining: rateLimitRemaining
-        });
-      } else {
-        // Reject the Promise with an error message
-        reject("Error: " + xhr.statusText);
-      }
-    };
-  });
+    const data = await response.json();
+    data.rateLimitRemaining = rateLimitRemaining; // Add rateLimitRemaining to the data object
+    return data;
+  } catch (error) {
+    console.error("Error fetching APOD data:", error);
+    throw error;
+  }
 }
