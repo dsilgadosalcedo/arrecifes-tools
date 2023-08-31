64ee30b6739904dabf7c7ccb865927d02692faaa
David Silgado <dsilgadosalcedo@gmail.com>
2023-08-30 23:55:34 -0500

    js: refactoring

---
+++ src/scripts/fetch.js
@@ -0,0 +1,59 @@
+const NASA_KEY='kmQOP13MoJhnRftKtbw8wEjK04O4xI6Dls8R0dc6'
+export const data = await getApodData()
+
+async function fetchApodData() {
+  const NASA_TOKEN = NASA_KEY
+  const apodApiUrl = `https://api.nasa.gov/planetary/apod?api_key=${NASA_TOKEN}`
+  
+  try {
+    const response = await fetch(apodApiUrl);
+    const rateLimitRemaining = response.headers.get("X-RateLimit-Remaining");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    
+    const data = await response.json();
+    data.rateLimitRemaining = rateLimitRemaining;
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+function getCurrentDate() {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, '0');
+  const day = String(today.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
+
+function getApodDataFromCache() {
+  let list = localStorage.getItem('listApodData');
+  list = list && JSON.parse(list);
+  return list
+}
+
+function getCurrentApodData() {
+  const list = getApodDataFromCache() ? getApodDataFromCache() : [];
+  const todayData = list && list.find(item => item.date === getCurrentDate());
+  return todayData ? todayData : null;
+}
+
+function saveApodDataToCache(apodData) {
+  const list = getApodDataFromCache() ? getApodDataFromCache() : [];
+  list.push(apodData);
+  localStorage.setItem('listApodData', JSON.stringify(list));
+}
+
+async function getApodData() {
+  const cachedApodData = getCurrentApodData();
+  if (cachedApodData && cachedApodData.date === getCurrentDate()) {
+    return cachedApodData;
+  } else {
+    const newApodData = await fetchApodData();
+    saveApodDataToCache(newApodData);
+    console.log(newApodData.rateLimitRemaining);
+    return newApodData;
+  }
+}
--- src/scripts/tools.ts
+++ src/scripts/tools.ts
@@ -1,54 +1,58 @@
-const cards = [
+// gradient
+type FromColor = `from-${string}`
+type ToColor = `to-${string}`
+
+interface Card {
+  title: string
+  description: string
+  url?: string
+  image: string
+  fromColor: FromColor
+  toColor: ToColor
+}
+
+export const cards: Card[] = [
   {
     title: "Space",
     description: "Nuestras herramientas celestiales.",
-    url: "#",
     image: "/icons/resources-icon.svg",
-    firstColor:"from-cold-400",
-    secondColor:"to-gray-800",
+    fromColor:"from-cold-400",
+    toColor:"to-gray-800",
   },
   {
     title: "Tratamientos",
     description: "Las Tools para el tratamiento de suelos y agua.",
-    url: "#",
     image: "/icons/pollution-icon.svg",
-    firstColor:"from-white",
-    secondColor:"to-gray-800",
+    fromColor:"from-white",
+    toColor:"to-gray-800",
   },
   {
     title: "Ecosistemas",
     description: "Tejidos vivos interconectados en equilibrio.",
     url: "/ecosystems",
     image: "/icons/ecosystems-icon.svg",
-    firstColor:"from-cold-400",
-    secondColor:"to-[#42cc80]",
+    fromColor:"from-cold-400",
+    toColor:"to-[#42cc80]",
   },
   {
     title: "Clima",
     description: "Entre el equilibrio y el caos.",
-    url: "#",
     image: "/icons/climate-icon.svg",
-    firstColor:"from-white",
-    secondColor:"to-cold-400",
+    fromColor:"from-white",
+    toColor:"to-cold-400",
   },
   {
     title: "Residuos",
     description: "Manejo integral de residuos aquí.",
-    url: "#",
     image: "/icons/waste-icon.svg",
-    firstColor:"from-[#42cc80]",
-    secondColor:"to-[#105136]",
+    fromColor:"from-[#42cc80]",
+    toColor:"to-[#105136]",
   },
   {
     title: "La Mapzone",
     description: "Explora el planeta en mapas.",
-    url: "#",
     image: "/icons/resources-icon.svg",
-    firstColor:"from-cold-400",
-    secondColor:"to-cold-600",
+    fromColor:"from-cold-400",
+    toColor:"to-cold-600",
   },
 ]   
-
-export function getTools() {
-  return cards;
-}
