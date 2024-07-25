336229c266c829cd289cc659a9fcb8962be7a60d
David Silgado Salcedo <dsilgadosalcedo@gmail.com>
2024-07-24 23:31:02 -0500

    Change theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,34 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
 			colors: {
 				cold: {
-					900: '#050412',
-					800: '#0a071c',
-					700: '#19164d',
-					600: '#332c61',
-					500: '#2c287d',
-					400: '#935085',
-					300: '#9380ff',
-					200: '#9c9eff',
+					900: "rgb(4, 5, 8)",
+					800: "rgb(9, 11, 17)",
+					700: "rgb(14, 18, 27)",
+					600: "rgb(20, 25, 37)",
+					500: "rgb(26, 32, 47)",
+					400: "rgb(32, 39, 57)",
+					300: "rgb(38, 46, 67)",
+					200: "rgb(44, 53, 77)",
+					100: "rgb(50, 60, 87)",
 				},
 				hot: {
-					900: '#f20103',
-					800: '#ff9885'
+					900: "#f20103",
+					800: "#ff9885",
 				},
-				white: '#93f0ff'
+				white: "#93f0ff",
 			},
 			fontFamily: {
-				'splineSans-bold': ['SplineSans-Bold', 'sans-serif'],
-				'splineSans-regular': ['SplineSans-Regular', 'sans-serif'],
-				'monoDisplay-normal': ['MonoDisplay-normal', 'mono']
-			}
+				"splineSans-bold": ["SplineSans-Bold", "sans-serif"],
+				"splineSans-regular": ["SplineSans-Regular", "sans-serif"],
+				"monoDisplay-normal": ["MonoDisplay-normal", "mono"],
+			},
 		},
-		
 	},
-	plugins: [
-		require('tailwindcss-animated')
-	],
+	plugins: [require("tailwindcss-animated")],
 }
