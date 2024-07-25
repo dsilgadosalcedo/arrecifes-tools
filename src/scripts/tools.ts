e09159751b9c312d95013fd077502622e71dadc7
David Silgado Salcedo <dsilgadosalcedo@gmail.com>
2024-07-25 01:09:33 -0500

    Update cards to project section

--- src/scripts/tools.ts
+++ src/scripts/tools.ts
@@ -3,56 +3,56 @@ type FromColor = `from-${string}`
 type ToColor = `to-${string}`
 
 interface Card {
-  title: string
-  description: string
-  url?: string
-  image: string
-  fromColor: FromColor
-  toColor: ToColor
+	title: string
+	description: string
+	url?: string
+	image: string
+	fromColor: FromColor
+	toColor: ToColor
 }
 
 export const cards: Card[] = [
-  {
-    title: "Space",
-    description: "Nuestras herramientas celestiales.",
-    image: "/icons/resources-icon.svg",
-    fromColor:"from-cold-400",
-    toColor:"to-gray-800",
-  },
-  {
-    title: "Tratamientos",
-    description: "Las Tools para el tratamiento de suelos y agua.",
-    image: "/icons/pollution-icon.svg",
-    fromColor:"from-white",
-    toColor:"to-gray-800",
-  },
-  {
-    title: "Ecosistemas",
-    description: "Tejidos vivos interconectados en equilibrio.",
-    url: "/ecosystems",
-    image: "/icons/ecosystems-icon.svg",
-    fromColor:"from-cold-400",
-    toColor:"to-[#42cc80]",
-  },
-  {
-    title: "Clima",
-    description: "Entre el equilibrio y el caos.",
-    image: "/icons/climate-icon.svg",
-    fromColor:"from-white",
-    toColor:"to-cold-400",
-  },
-  {
-    title: "Residuos",
-    description: "Manejo integral de residuos aquí.",
-    image: "/icons/waste-icon.svg",
-    fromColor:"from-[#42cc80]",
-    toColor:"to-[#105136]",
-  },
-  {
-    title: "La Mapzone",
-    description: "Explora el planeta en mapas.",
-    image: "/icons/resources-icon.svg",
-    fromColor:"from-cold-400",
-    toColor:"to-cold-600",
-  },
-]   
+	{
+		title: "Space",
+		description: "Nuestras herramientas celestiales.",
+		image: "/icons/resources-icon.svg",
+		fromColor: "from-cold-100",
+		toColor: "to-cold-800",
+	},
+	// {
+	// 	title: "Tratamientos",
+	// 	description: "Las Tools para el tratamiento de suelos y agua.",
+	// 	image: "/icons/pollution-icon.svg",
+	// 	fromColor: "from-white",
+	// 	toColor: "to-gray-800",
+	// },
+	{
+		title: "Ecosistemas",
+		description: "Tejidos vivos interconectados en equilibrio.",
+		url: "/ecosystems",
+		image: "/icons/ecosystems-icon.svg",
+		fromColor: "from-[#42cc80]",
+		toColor: "to-cold-400",
+	},
+	{
+		title: "Clima",
+		description: "Entre el equilibrio y el caos.",
+		image: "/icons/climate-icon.svg",
+		fromColor: "from-white",
+		toColor: "to-cold-400",
+	},
+	// {
+	// 	title: "Residuos",
+	// 	description: "Manejo integral de residuos aquí.",
+	// 	image: "/icons/waste-icon.svg",
+	// 	fromColor: "from-[#42cc80]",
+	// 	toColor: "to-[#105136]",
+	// },
+	// {
+	//   title: "La Mapzone",
+	//   description: "Explora el planeta en mapas.",
+	//   image: "/icons/resources-icon.svg",
+	//   fromColor:"from-cold-400",
+	//   toColor:"to-cold-600",
+	// },
+]
